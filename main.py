81331561d2279adaf1137a1a09e7c5515ddd8520
Ariledla <arielsaradilla7@gmail.com>
2026-06-11 06:10:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from opensearchpy import OpenSearch
 
 app = FastAPI(
     title="QA System Rumah Sakit Sehat Selalu",
     version="1.0"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Request Model
